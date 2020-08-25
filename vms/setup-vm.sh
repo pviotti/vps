b2278d35ddb9419de20268d0eb9f410674258b4a
Paolo <pviotti@users.noreply.github.com>
2020-08-25 21:18:10 +0100

    remove docker user setting from script

--- vms/setup-vm.sh
+++ vms/setup-vm.sh
@@ -7,7 +7,6 @@ sudo apt install -y vim htop screen git make
 
 echo "Install Docker..."
 curl -sSL https://get.docker.com/ | sh
-sudo usermod -aG docker $USER
 
 echo "Install Docker Compose..."
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -19,4 +18,4 @@ sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
 sudo ufw enable
 
-echo "Done! (remember to set up password-less authentication!)"
+echo "Done!"
